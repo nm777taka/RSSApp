c60bb59b7d5bd7a412e893f012c85a984b7363d0
TakahisaFuruta <nm7.taka@gmail.com>
2014-05-31 17:05:36 +0900

    FeedListViewとFeedViewを追加

---
+++ AGNews/AGFeedListViewController.h
@@ -0,0 +1,13 @@
+//
+//  AGFeedListViewController.h
+//  AGNews
+//
+//  Created by TakahisaFuruta on 2014/05/31.
+//  Copyright (c) 2014年 TakahisaFuruta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AGFeedListViewController : UIViewController
+
+@end
